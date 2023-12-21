36a5c09d667fb31d34ade52c463b9d81e032a71e
bello1470 <126983812+bello1470@users.noreply.github.com>
2023-12-21 16:44:53 +0000

    MergeSort day of Commits

---
+++ src/main/java/org/bellotech/LinkedList/LinkedList.java
@@ -0,0 +1,16 @@
+package org.bellotech.LinkedList;
+
+public class LinkedList {
+    Node head;
+
+    public void insert(int data){
+        Node node = new Node();
+        node.data = data;
+        if (head == null){
+            head = node;
+        }else {
+
+            Node n = head;
+        }
+    }
+}
---
+++ src/main/java/org/bellotech/LinkedList/Main.java
@@ -0,0 +1,8 @@
+package org.bellotech.LinkedList;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
